13a686465483119e3739084ccb53a86a5960c80c
hieple <hieplevuc@gmail.com>
2023-12-20 06:13:17 +0700

    setup abstract repository for database

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -2,24 +2,12 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { User, UserDocument } from "./schemas/user.schema";
 import { FilterQuery, Model } from "mongoose";
+import { EntityRepository } from "src/database/entity.repository";
 
 @Injectable()
-export class UsersRepository {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
-    
-  }
-  async findOne(userFilterQuery: FilterQuery<User>): Promise<User> {
-    return this.userModel.findOne(userFilterQuery);
-  }
-  async find(usersFilterQuery: FilterQuery<User>): Promise<User[]>{
-    return this.userModel.find(usersFilterQuery);
-  }
-  async create(user: User): Promise<User> {
-    const newUser = new this.userModel(user);
-    return newUser.save()
+export class UsersRepository extends EntityRepository<UserDocument> {
+  constructor(@InjectModel(User.name) userModel: Model<UserDocument>) {
+    super(userModel);
   }
 
-  async findOneAndUpdate(userFilterQuery: FilterQuery<User>, user: Partial<User>): Promise<User> {
-    return this.userModel.findOneAndUpdate(userFilterQuery, user, { new: true });
-  }
 }
